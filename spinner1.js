62e034e62decfe7f3ed99dc7cc6b35124d8aed5e
trevorfunk <trevorjonfunk@gmail.com>
2022-06-09 19:43:38 -0700

    full rotation of spinner

---
+++ spinner1.js
@@ -0,0 +1,19 @@
+setTimeout(() => {
+ process.stdout.write('\r|  ');
+}, 100);
+
+setTimeout(() => {
+ process.stdout.write('\r/  ');
+}, 400);
+
+setTimeout(() => {
+ process.stdout.write('\r-  ');
+}, 700);
+
+setTimeout(() => {
+ process.stdout.write('\r\\  '); 
+}, 1000);
+
+setTimeout(() => {
+ process.stdout.write('\r\|  '); 
+}, 1300);
